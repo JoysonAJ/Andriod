de713f267d1d6f1da9de0ec9822c59eeb380d0a0
AJ <joysondsouzaaj@gmail.com>
2022-07-19 19:22:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/projectapp/HomePage.java
@@ -0,0 +1,39 @@
+package com.example.projectapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomePage extends AppCompatActivity {
+    Button logout, framentButton1, framentButton2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        logout = findViewById(R.id.BackLogin);
+        framentButton1 = findViewById(R.id.Fragment1);
+        framentButton2 = findViewById(R.id.Fragment2);
+
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        framentButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this,Fragment1.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
